46912c51b3116812f8a88081416fd1bfa2a74d2a
nabinmillias <nabin.illias@gmail.com>
2019-06-10 16:15:04 +0530

    alert added

--- app/src/main/java/com/example/employeeretrofit/MainActivity.java
+++ app/src/main/java/com/example/employeeretrofit/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.employeeretrofit;
 
 import android.app.AlertDialog;
-import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.employeeretrofit.Mofdels.EmployeeList;
@@ -20,20 +16,22 @@ import dmax.dialog.SpotsDialog;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-    AlertDialog alertDialog;
+   // SpotsDialog.Builder alertDialog;
 
+    AlertDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        dialog=new SpotsDialog(this);
        // alertDialog=new  SpotsDialog(this);
 
+dialog.show();
 
         final RecyclerView recyclerview = findViewById(R.id.rvxml);
         recyclerview.setHasFixedSize(true);
@@ -52,10 +50,13 @@ public class MainActivity extends AppCompatActivity {
 
                     DataAdapter adpt=new DataAdapter((ArrayList<EmployeeList>) response.body(),MainActivity.this);
                     recyclerview.setAdapter(adpt);
+
+                    dialog.dismiss();
                 }
 
                 @Override
                 public void onFailure(Call<List<EmployeeList>> call, Throwable t) {
+                    dialog.dismiss();
                     Toast.makeText(MainActivity.this, "failure", Toast.LENGTH_SHORT).show();
                 }
             });
